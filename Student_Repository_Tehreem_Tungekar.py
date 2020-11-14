a13bfda08d376b315f94293ac464074ad5c6aefe
Tehreem Tungekar <61510365+tehreemt@users.noreply.github.com>
2020-11-14 17:25:17 -0500

    Update Student_Repository_Tehreem_Tungekar.py
    Add the new functionality to compute the student's GPA
    Add new functionality to read the majors file and calculate the remaining required and elective classes for each student
    Add a new Majors prettytable
    Update the Student prettytable to include the student's GPA and remaining classes and electives for each student
    Implement automated tests to verify that the data in the prettytables matches the data from the input data files.

--- Student_Repository_Tehreem_Tungekar.py
+++ Student_Repository_Tehreem_Tungekar.py
@@ -1,40 +1,17 @@
 """
 @author: Tehreem Tungekar
-There are three parts in this assignment:
-Part 1: Date Arithmetic Operations
-Part 2: field separated file reader
-Part 3: Scanning directories and files
+The main objective of this program is to
+begin to build the framework for a project and
+summarize student and instructor data
+This program has a class named University,
+a class named Student and another named Instructor
+Added a new class named Majors
 """
-import csv
-import os
+
 from prettytable import PrettyTable
-from datetime import datetime, timedelta
-from typing import Tuple, Iterator
-
-
-def date_arithmetic() -> Tuple[datetime, datetime, int]:
-    """ This function answers the following questions and returns a tuple:
-1. What is the date three days after Feb 27, 2020?
-2. What is the date three days after Feb 27, 2019?
-3. How many days passed between Feb 1, 2019 and Sept 30, 2019?
-"""
-    date1: str = "Feb 27, 2020"
-    date2: str = "Feb 27, 2019"
-    date3: str = "Feb 1, 2019"
-    date4: str = "Sep 30, 2019"
-
-    dt1: datetime = datetime.strptime(date1, "%b %d, %Y")
-    dt2: datetime = datetime.strptime(date2, "%b %d, %Y")
-    dt3: datetime = datetime.strptime(date3, "%b %d, %Y")
-    dt4: datetime = datetime.strptime(date4, "%b %d, %Y")
-
-    num_days: int = 3
-    res1: datetime = dt1 + timedelta(num_days)
-    res2: datetime = dt2 + timedelta(num_days)
-
-    delta: datetime = dt4 - dt3
-    res3: int = delta.days
-    return res1, res2, res3
+import csv
+from collections import defaultdict
+from typing import Tuple, Iterator, List, Set, Dict
 
 
 def file_reader(
@@ -48,7 +25,7 @@ def file_reader(
         fp = open(path, 'r')
 
     except FileNotFoundError:
-        raise FileNotFoundError("File not found!")
+        print("File not found!")
 
     else:
         line_num: int = 0
@@ -57,75 +34,238 @@ def file_reader(
             for line in reader:
                 line_num += 1
                 if len(line) != fields:
-                    raise ValueError(
+                    print(
                         f'{path} has {len(line)} fields on',
                         'line {line_num} expected {fields}')
                 if header is False:
                     yield tuple(line)
                 else:
                     header = False
-        fp.close()
 
 
-class FileAnalyzer:
-    """ This class when given a directory name,
-    searches that directory for Python files.
-    For each file, opens it and calculates
-    a summary of the file
+class University:
+    """ This class holds all of  the students,
+    instructors and grades for a single University
     """
-    def __init__(self, directory: str) -> None:
-        """ """
-        self.directory: str = directory
-        self.files_summary: Dict[str, Dict[str, int]] = dict()
+    def __init__(self, path: str, tables: bool = True) -> None:
+        """Init method for University"""
+        self._path: str = path
+        self._students: Dict[str, Student] = dict()
+        self._instructors: Dict[str, Instructor] = dict()
+        self._major: Dict[str, Major] = dict()
 
-        self.analyze_files()
+        student_path = "students.txt"
+        instructor_path = "instructors.txt"
+        grades_path = "grades.txt"
+        major_path = "majors.txt"
 
-    def analyze_files(self) -> None:
-        """ This is a method that populates the summarized
-        data into self.files_summary
+        try:
+            """It is important to execute get_major()
+            before get_students() to avoid errors
+            """
+            self._get_major(major_path)
+            self._get_students(student_path)
+            self._get_instructors(instructor_path)
+            self._get_grades(grades_path)
+
+        except (FileNotFoundError, ValueError) as e:
+            print(e)
+
+        else:
+            if tables:
+                print("\nMajors Table:")
+                self.major_table()
+                print("\nStudent Table:")
+                self.student_table()
+                print("\nInstructor Table:")
+                self.instructor_table()
+
+    def _get_students(self, path) -> None:
+        """Student's details are read using file_reader method
+        and added to students dictionary
         """
-        path: str = self.directory
-        direct: List[str] = os.listdir(path)
-        for f in direct:
-            if f.endswith('.py'):
-                try:
-                    fp = open(os.path.join(path, f), 'r')
-                except FileNotFoundError:
-                    raise FileNotFoundError("Unable to open file!")
-                else:
-                    with fp:
-                        character_count: int = 0
-                        class_count: int = 0
-                        function_count: int = 0
-                        line_count: int = 0
+        for cwid, name, major in file_reader(path, 3, sep=';', header=True):
+            if major not in self._major:
+                print(f"Student {cwid} '{name}' has unknown major '{major}'")
+            else:
+                self._students[cwid] = Student(cwid, name, self._major[major])
 
-                        for line in fp:
-                            character_count += len(line)
-                            line_count += 1
+    def _get_instructors(self, path) -> None:
+        """Instructor's details are read using
+        file_reader method and added to instructor dictionary"""
+        for cwid, name, dept in file_reader(path, 3, sep='|', header=True):
+            self._instructors[cwid] = Instructor(cwid, name, dept)
 
-                            if line.strip().startswith('def '):
-                                function_count += 1
-                            if line.startswith('class '):
-                                class_count += 1
+    def _get_grades(self, path) -> None:
+        """Grades are read using file_reader
+        method and assigned to student and instructor"""
+        for std_cwid, course, grade, instructor_cwid in file_reader(
+                                            path, 4, sep='|', header=True):
+            if std_cwid in self._students:
+                self._students[std_cwid].add_course(course, grade)
+            else:
+                print(f'Student with {std_cwid} not found in Student file')
 
-                        self.files_summary[f] = {
-                            'classes': class_count,
-                            'functions': function_count,
-                            'lines': line_count,
-                            'characters': character_count
-                        }
+            if instructor_cwid in self._instructors:
+                self._instructors[instructor_cwid].add_student(course)
+            else:
+                print(f'Instructor {instructor_cwid} not in Instructor File')
 
-    def pretty_print(self) -> None:
-        """ This is a method that prints out the pretty table
-        from the data stored in the self.files_summary
+    def _get_major(self, path) -> None:
+        """Majors are read using file_reader method and students summary
+        table has to be updated using the same information
         """
-        pretty: PrettyTable = PrettyTable()
-        pretty.field_names = ['File Name', 'Classes',
-                              'Functions', 'Lines', 'Characters']
+        for major, flag, course in file_reader(path, 3, sep='\t', header=True):
+            if major not in self._major:
+                self._major[major] = Major(major)
+            self._major[major].add_course(course, flag)
 
-        for i, j in self.files_summary.items():
-            pretty.add_row([i, j['classes'], j['functions'],
-                            j['lines'], j['characters']])
+    def student_table(self) -> None:
+        """This function Pretty Prints Student table"""
+        table = PrettyTable(field_names=Student.FIELD_NAMES)
+        for student in self._students.values():
+            table.add_row(student.info())
+        print(table)
 
-        print("\nSummary for ", self.directory)
-        print(pretty)
+    def instructor_table(self) -> None:
+        """This function Pretty Prints Instructor table"""
+        table = PrettyTable(field_names=Instructor.FIELD_NAMES)
+        for instructor in self._instructors.values():
+            for row in instructor.info():
+                table.add_row(row)
+        print(table)
+
+    def major_table(self) -> None:
+        """This table Pretty Print Majors table"""
+        table = PrettyTable(field_names=Major.FIELD_NAMES)
+        for major in self._major.values():
+                table.add_row(major.info())
+        print(table)
+
+
+class Student:
+    """This class holds all of the details of a student,
+    including a dict to store the classes taken and the grade
+    where the course is the key and the grade is the value
+    """
+
+    FIELD_NAMES: List[str] = ['CWID', 'Name', 'Major',
+                              'Completed Courses', 'Remaining Required',
+                              'Remaining Electives', 'GPA']
+
+    def __init__(self, cwid: str, name: str, major: str) -> None:
+        """Init method for Student class"""
+        self._cwid: str = cwid
+        self._name: str = name
+        self._major: str = major
+        self._courses: Dict[str, str] = dict()
+
+    def add_course(self, course: str, grade: str) -> None:
+        """Adding courses with grades"""
+        self._courses[course] = grade
+
+    def gpa(self) -> float:
+        """This method is calculating the GPA using a dictionary"""
+        grades: Dict[str, float] = {'A': 4.0, 'A-': 3.75, 'B+': 3.25,
+                                    'B': 3.0, 'B-': 2.75,
+                                    'C+': 2.25, 'C': 2.0,
+                                    'C-': 0.00, 'D+': 0.00,
+                                    'D': 0.00, 'D-': 0.00, 'F': 0.00}
+        try:
+            total: float = sum(
+                [grades[grade]
+                    for grade in self._courses.values()]
+                ) / len(self._courses.values())
+            return round(total, 2)
+        except ZeroDivisionError as e:
+            print(e)
+
+    def info(self) -> Tuple[str, str, str, List[str], List[str], List[str]]:
+        """This method returns a list of information needed for generating
+        pretty table """
+        major, passed, remaining, electives = self._major.remaining(
+            self._courses)
+
+        return [self._cwid, self._name, major,
+                sorted(passed), sorted(remaining),
+                sorted(electives), self.gpa()]
+
+
+class Instructor:
+    """This class holds all of the details of an instructor,
+    including a defaultdict(int) to store the names of the courses
+    taught along with the number of students who have taken the course
+    """
+    FIELD_NAMES = ['CWID', 'Name', 'Dept', 'Course', 'Number of Students']
+
+    def __init__(self, cwid: str, name: str, dept: str) -> None:
+        """Init method for Instructor Class
+        """
+        self._cwid: str = cwid
+        self._name: str = name
+        self._dept: str = dept
+        self._courses: DefaultDict[str, int] = defaultdict(int)
+
+    def add_student(self, course: str) -> None:
+        """Number of students taking course with Instructor"""
+        self._courses[course] += 1
+
+    def info(self) -> Iterator[Tuple[str, str, str, str, int]]:
+        """Yields the row of Instructors"""
+        for course, count in self._courses.items():
+            yield [self._cwid, self._name, self._dept, course, count]
+
+
+class Major:
+    """This class holds all the information
+    about majors
+    """
+    FIELD_NAMES = ['Major', 'Required Courses', 'Electives']
+    GRADES: Set[str] = {'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C'}
+
+    def __init__(self, major: str) -> None:
+        """
+        Init method for Major class
+        """
+        self._major: str = major
+        self._required: Set[str] = set()
+        self._electives: Set[str] = set()
+
+    def add_course(self, course: str, option: str) -> None:
+        """Adding a course by determining
+        if it is elective or required
+        """
+        if option == 'R':
+            self._required.add(course)
+        elif option == 'E':
+            self._electives.add(course)
+        else:
+            print("Invalid Course Flag!")
+
+    def remaining(
+            self,
+            completed: Dict[str, str]) -> Tuple[str,
+                      List[str], List[str], List[str]]:
+        """Adding remaining courses not passed/enrolled in and electives"""
+        passed: Set[str] = {course for course,
+                            grade in completed.items()
+                            if grade in Major.GRADES}
+        remaining: Set[str] = self._required - passed
+        electives: Set[str] = self._electives
+
+        if self._electives.intersection(passed):
+            electives = []
+
+        return self._major, list(passed), list(remaining), list(electives)
+
+    def info(self) -> Tuple[str, List[str], List[str]]:
+        """ Returning information about majors"""
+        return [self._major, sorted(self._required), sorted(self._electives)]
+
+
+def main():
+    University('Homework_10')
+
+
+if __name__ == '__main__':
+    main()
